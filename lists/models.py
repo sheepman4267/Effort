142dd5ea3676daffb4101dbfee48cbc2a44bab94
sheepman4267 <hans@communitycomputing.net>
2022-04-26 21:09:26 -0400

    Begin adding to models and admin

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,8 @@
-import datetime
 from django.contrib.auth.models import User
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 from markdownx.models import MarkdownxField
 
 import datetime
@@ -16,6 +18,17 @@ class ListItem(models.Model):
     due_date = models.DateTimeField()
     created_date = models.DateTimeField(default=datetime.datetime.now(), editable=False)
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    quick_access = models.ManyToManyField(List, related_name='quick_access')
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 # Create your models here.
