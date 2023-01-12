4632fabeb86e70c6f720879e749ec671347e2926
Hans K <hans@communitycomputing.net>
2023-01-11 21:34:19 -0500

    Whoops! We do still need that other path.

--- lists/urls.py
+++ lists/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('list/<int:list>', views.display_list, name='list'),
     path('list/new', views.new_list, name='new-list'),
     path('list/new/<int:parent>', views.new_list, name='new-sublist'),
+    path('item/edit', views.item_edit, name='item-edit'),
     path('item/edit/<int:item_pk>', views.item_edit, name='item-edit'),
     path('item/<int:item_pk>/details', views.item_details, name='item-details'),
     path('item/toggle/<int:list_pk>/<int:item>', views.toggle_item, name='toggle-item'),
