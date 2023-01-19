36f411e82e41765c22c77aeb7228712f15e134bf
Hans K <hans@communitycomputing.net>
2023-01-18 21:48:58 -0500

    Write a basic test case for lists.views.display_list

--- lists/test_views.py
+++ lists/test_views.py
@@ -3,6 +3,8 @@ from django.test import Client
 from django.shortcuts import reverse
 from django.contrib.auth.models import User
 
+from lists.models import List, ListItem
+
 
 class IndexCase(TestCase):
     def setUp(self) -> None:
@@ -11,5 +13,18 @@ class IndexCase(TestCase):
         client.login(username='test_user', password='test_password')
         self.index = client.get(reverse('lists-index'))
 
-    def test_http_ok(self) -> None:
+    def test_http_response(self) -> None:
         self.assertEqual(self.index.status_code, 200)
+
+class DisplayListCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = User.objects.create_user(username='test_user', password='test_password')
+        self.client.login(username='test_user', password='test_password')
+        self.test_list = List.objects.create(title='test_list', owner=self.user)
+
+    def test_http_response(self) -> None:
+        existing_list_response = self.client.get(reverse('list', args=[self.test_list.pk]))
+        nonexisting_list_response = self.client.get(reverse('list', args=[0]))
+        self.assertEqual(existing_list_response.status_code, 200)
+        self.assertEqual(nonexisting_list_response.status_code, 404)
