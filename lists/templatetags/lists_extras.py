ab530acc0cb633ed2ec2075a64fdb113286a1dca
Hans K <hans@communitycomputing.net>
2023-01-31 22:02:16 -0500

    Format lists/templatetags/lists_extras.py using `black`

--- lists/templatetags/lists_extras.py
+++ lists/templatetags/lists_extras.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 # 'list' is the one we're providing a button for.
 # 'selected_list' is the list that is currently being displayed.
-@register.inclusion_tag('lists/list-selection-display.html')
+@register.inclusion_tag("lists/list-selection-display.html")
 def list_selection_display(list, selected_list, request):
     if list in selected_list.tree():
         expand_tree = True
@@ -12,23 +12,21 @@ def list_selection_display(list, selected_list, request):
         expand_tree = True
     else:
         expand_tree = False
-    return({
-        'list': list,
-        'expand_tree': expand_tree,
-        'selected_list': selected_list,
-        'request': request,
-    })
+    return {
+        "list": list,
+        "expand_tree": expand_tree,
+        "selected_list": selected_list,
+        "request": request,
+    }
 
 
-@register.inclusion_tag('lists/star.html')
+@register.inclusion_tag("lists/star.html")
 def star(request, todo):
     if request.user in todo.starred.filter():
-        star_button_fill = '#ffd500'
+        star_button_fill = "#ffd500"
     else:
-        star_button_fill = 'transparent'
-    return({
-        'star_button_fill': star_button_fill,
-        'todo': todo,
-    })
-
-
+        star_button_fill = "transparent"
+    return {
+        "star_button_fill": star_button_fill,
+        "todo": todo,
+    }
